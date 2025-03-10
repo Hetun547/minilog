e0619c88625c6dd1ecb246f124326b597805a4a4
Hetun547 <62554659+Hetun547@users.noreply.github.com>
2025-03-10 18:02:02 +0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,22 +1,9 @@
-﻿
+
 #include "minilog.h"
 
 #include <cstdio>
 #include <iostream>
 #include <tuple>
-template <typename... Args>
-void print(char* format, Args... args) {
-	//auto tt = std::make_tuple(args...);
-	//if constexpr (std::tuple_size_v<decltype(tt)> > 1) {
-	//	auto sdd = std::get<1>(tt);
-	//	if constexpr (std::is_same_v<char, decltype(sdd)> || std::is_convertible_v<decltype(sdd), std::string>)
-	//		std::cout << sdd << std::endl;
-	//}
-	char sd[128] = { 0 };
-	std::sprintf(sd, format, args...);
-	
-	return;
-}
 
 #include <chrono>
 #include <thread>
@@ -25,7 +12,7 @@ int main()
 {
 	auto path = std::filesystem::current_path();
 	path.append("log");
-	minilog::init(path.string().c_str());
+	minilog::init(path.string().c_str(), 1);
 	srand(time(0));
 	for (int i = 0; i < 100; ++i) {
 		error("sds", "%d\n", rand());
